615742d4b5fc7d28d4a4504cfad65c20b3e0067c
Una LEstrange <lestrange.una@itsligo.ie>
2021-09-23 15:39:44 +0100

    changed the books array to have a proper id

--- index.js
+++ index.js
@@ -5,9 +5,22 @@ const port = 3000
 // configure the middleware for parsing HTML requeest body
 
 app.use(express.json());
-app.use(express.urlencoded({extended: false})); //Parse URL-encoded bodies
+app.use(express.urlencoded({ extended: false })); //Parse URL-encoded bodies
 
-let books = [];
+let books = [
+  {
+    "id": 1,
+    "name": "Gansta Granny"
+  },
+  {
+    "id": 2,
+    "name": "The Boy in the Dress"
+  },
+  {
+    "id": 3,
+    "name": "Bad Dad"
+  },
+];
 
 app.get('/', (req, res) => res.send('Hello World from Una using nodemon!'))
 
@@ -15,49 +28,56 @@ app.get('/bananas', (req, res) =>
   res.send('hello world, this is bananas'));
 
 
-  app.post('/books', (req, res) => {
+app.post('/books', (req, res) => {
 
-    const book = req.body;
-    
-    const bookNumber = books.length;
+  const newBookId = books.length;
 
-    books.push(book);
+  const book = {id: newBookId, ...req.body};
 
-    res.location(`/books/${bookNumber}`)
+  books.push(book);
+
+  res.location(`/books/${newBookId}`)
     .status(201)
     .json(book);
 
 
-    console.log(`book name is ${book.name} number of book(s) is ${books.length}`);
+  console.log(`book name is ${book.name} number of book(s) is ${books.length}`);
 
 });
 
-  app.get('/books', (req, res) => {
-      res.json(books);
-  })
+app.get('/books', (req, res) => {
+  res.json(books);
+})
 
 
-app.get('/books/:id', (req,res) => {
+app.get('/books/:id', (req, res) => {
 
-    let id = req.params.id;
+  const id = req.params.id;
 
-    if (id >= books.length) 
-    {
-      res.status(404);
-      res.json({error: 'not found'})
-    }
+  const book = books.find(b => b.id === parseInt(req.params.id))
 
-     res.json(books[id]);
- })
+  if (!book) {
+    res.status(404);
+    res.json({ error: 'not found' });
+    return 
+  }
+
+  res.json(book);
+})
 
 
-app.delete('/books/:id',(req, res) => {
-    let id = req.params.id; 
-    console.log(`removing book ${books[id].name}`)
-    books.splice(req.params.id, 1);
-    res.send(books);
+app.delete('/books/:id', (req, res) => {
 
-  })
+  const id = req.params.id;
+
+  const book = books.find(b => b.id === parseInt(req.params.id))
+
+  let id = req.params.id;
+  console.log(`removing book ${books[id].name}`)
+  books.splice(req.params.id, 1);
+  res.send(books);
+
+})
 
 
 

77e9d96bd8e3d09941491487923a70fbc9ef1e78
Una LEstrange <lestrange.una@itsligo.ie>
2021-10-17 21:12:00 +0100

    tidied up

--- middleware/jwtvalidation.js
+++ middleware/jwtvalidation.js
@@ -7,33 +7,12 @@ const  crypto = require( 'crypto');
 
 let secret =  'unasverySecretSecret' // would normally import this from a config file
 
-function verifyRefreshBodyField (req, res, next) {
-    if (req.body && req.body.refresh_token) {
-        return next();
-    } else {
-        return res.status(400).send({error: 'need to pass refresh_token field'});
-    }
-};
 
-function validRefreshNeeded  (req, res, next)  {
-   
-    let refresh_token = req.body.refresh_token;
-    let hash = crypto.createHmac('sha512', req.jwt.refreshKey).update(req.jwt.userId + secret).digest("base64");
-    
-    let b = Buffer.from(hash);
-    let token = b.toString('base64');
-    if (token === refresh_token) {
-        req.auth = req.jwt;
-        return next();
-    } else {
-        return res.status(400).send({error: 'Invalid refresh token'});
-    }
-};
 
 
 // this 
 // a) checks that there is a Bearer token in the authorization header
-// b) the token is valid using the signature
+// b) the token is valid using the secret
 
 function validJWTNeeded (req, res, next) {
     if (req.headers['authorization']) {
@@ -54,4 +33,4 @@ function validJWTNeeded (req, res, next) {
     }
 };
 
-module.exports = { validRefreshNeeded, validJWTNeeded, verifyRefreshBodyField}
+module.exports = { validJWTNeeded, }
